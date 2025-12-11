0990e92620610df9da3210bd267a0289755edf74
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-12-11 18:38:08 +0100

    add navbar with logo and pages links

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TomTruc</title>
+    <link rel="icon" type="image/png" href="img/icon.png">
+    <link rel="stylesheet" href="/TomTroc/css/styles.css">
+</head>
+<body>
+    <header>
+    <nav class="navbar">
+        <div class="left-nav">
+            <img src="/TomTroc/img/logo.svg" alt="Logo">
+            <div class="center-nav">
+                <a href="/TomTroc/index.php" class="navlink">Accueil</a>
+                <a href="/TomTroc/views/templates/exchangeBooks.php" class="navlink">Nos livres à l'échange</a>
+            </div>
+        </div>
+        <div class="right-nav">
+            <div>
+                <img src="/TomTroc/img/icon_messagerie.svg" alt="Messagerie">
+                <a href="/TomTroc/views/templates/messages.php" class="navlink">Messagerie</a>
+            </div>
+            <div>
+                <img src="/TomTroc/img/icon_account.svg" alt="Compte">
+                <a href="/TomTroc/views/templates/accountPage.php" class="navlink">Mon compte</a>
+            </div>
+            <div>
+                <a href="/TomTroc/views/templates/login.php" class="navlink">Connexion</a>
+            </div>
+        </div>
+    </nav>
+    </header>
+</body>
+</html>
