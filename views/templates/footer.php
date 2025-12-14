8b21f39df1aa876293610cfb4c7daa9b75f1e05e
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-12-14 18:34:20 +0100

    separate footer in file

---
+++ views/templates/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p class="footer-content">Politique de confidentialité</p>
+    <p class="footer-content">Mentions légales</p>
+    <p class="footer-content">Tom Troc©</p>
+    <a href="/TomTroc/index.php"><img id="footer_image" src="/TomTroc/img/footer_image.png" alt="Footer Image"></a>
+</footer>
