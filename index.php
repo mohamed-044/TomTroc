d0181d90003fd0259a1ab669857c56e4a032d127
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-12-14 21:33:01 +0100

    add registation page

--- index.php
+++ index.php
@@ -8,9 +8,10 @@
     <link rel="stylesheet" href="/TomTroc/css/styles.css">
 </head>
 <body>
-    <!-- <?php include 'views/templates/main.php'; ?> -->
+    <?php include 'views/templates/main.php'; ?>
     <!-- <?php include 'views/templates/exchangeBooks.php'; ?> -->
      <!-- <?php include 'views/templates/detailBook.php'; ?> -->
-      <?php include 'views/templates/connectionForm.php'; ?>
+      <!-- <?php include 'views/templates/connectionForm.php'; ?> -->
+        <!-- <?php include 'views/templates/registrationForm.php'; ?> -->
 </body>
 </html>
---
+++ views/templates/registrationForm.php
@@ -0,0 +1,23 @@
+<?php include __DIR__ . '/header.php'; ?>
+
+<section class="connect-section">
+    <div class="connect-content">
+        <h1 class="title">Inscription</h1>
+        <form class="connection-form" action="/TomTroc/index.php" method="post">
+            <label class="author" for="pseudo">Pseudo</label>
+            <input type="text" id="pseudo" name="pseudo" required>
+
+            <label class="author" for="email">Adresse e-mail</label>
+            <input type="email" id="email" name="email" required>
+
+            <label class="author" for="password">Mot de passe</label>
+            <input type="password" id="password" name="password" required>
+
+            <button type="submit" class="cta-button">S'inscrire</button>
+        </form>
+        <p class="signup-text">Déjà inscrit ? <a class="signup-link" href="/TomTroc/views/templates/connectionForm.php">Connectez-vous</a></p>
+    </div>
+    <img id="connect_image" src="/TomTroc/img/hero_image.jpg" alt="Hero Image">
+</section>
+
+<?php include __DIR__ . '/footer.php'; ?>
