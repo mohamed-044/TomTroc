ac4861b527b39b2042c7783986ac9ff80e7f127b
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-12-14 18:34:31 +0100

    separate header in file

---
+++ views/templates/header.php
@@ -0,0 +1,24 @@
+<header>
+    <nav class="navbar">
+        <div class="left-nav">
+            <a href="/TomTroc/index.php"><img src="/TomTroc/img/logo.svg" alt="Logo"></a>
+            <div class="center-nav">
+                <a href="/TomTroc/index.php" class="navlink">Accueil</a>
+                <a href="/TomTroc/views/templates/exchangeBooks.php" class="navlink">Nos livres à l'échange</a>
+            </div>
+        </div>
+        <div class="right-nav">
+            <div>
+                <img src="/TomTroc/img/icon_messagerie.svg" alt="Messagerie">
+                <a href="/TomTroc/views/templates/messages.php" class="navlink">Messagerie</a>
+            </div>
+            <div>
+                <img src="/TomTroc/img/icon_account.svg" alt="Compte">
+                <a href="/TomTroc/views/templates/accountPage.php" class="navlink">Mon compte</a>
+            </div>
+            <div>
+                <a href="/TomTroc/views/templates/login.php" class="navlink">Connexion</a>
+            </div>
+        </div>
+    </nav>
+    </header>
