ecb7e57d052ca97a6c4fbf14b8bc1441745703c7
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-12-16 21:00:56 +0100

    add account page

--- index.php
+++ index.php
@@ -8,10 +8,11 @@
     <link rel="stylesheet" href="/TomTroc/css/styles.css">
 </head>
 <body>
-    <?php include 'views/templates/main.php'; ?>
+    <!-- <?php include 'views/templates/main.php'; ?> -->
     <!-- <?php include 'views/templates/exchangeBooks.php'; ?> -->
      <!-- <?php include 'views/templates/detailBook.php'; ?> -->
       <!-- <?php include 'views/templates/connectionForm.php'; ?> -->
         <!-- <?php include 'views/templates/registrationForm.php'; ?> -->
+         <?php include 'views/templates/accountPage.php'; ?>
 </body>
 </html>
---
+++ views/templates/accountPage.php
@@ -0,0 +1,81 @@
+<?php include __DIR__ . '/header.php'; ?>
+
+<section class="account-section">
+    <h1 class="title5">Mon Compte</h1>
+    <div class="infos-section">
+        <div class="account-content">
+            <img id="account_image" src="/TomTroc/img/section_image.jpg" alt="Account Image">
+            <a class="modify-text">modifier</a>
+            <img src="/TomTroc/img/line2.png" alt="Line Image">
+            <p class="account-name">nathalire</p>
+            <p class="account-info">Membre depuis 1 ans</p>
+            <p class="little-text">BIBLIOTHEQUE</p>
+            <div class="books-number"><img src="/TomTroc/img/books_icon.svg" alt="Books Icon"><p id="book-text">4 Livres</p></div>
+
+        </div>
+        <div class="account-actions">
+            <p class="account-link">Vos informations personnelles</p>
+            <form class="account-form" action="/TomTroc/index.php" method="post">
+
+            <label class="author" for="email">Adresse e-mail</label>
+            <input type="email" id="email" name="email" class="account-input" required>
+
+            <label class="author" for="password">Mot de passe</label>
+            <input type="password" id="password" name="password" class="account-input" required>
+
+            <label class="author" for="pseudo">Pseudo</label>
+            <input type="text" id="pseudo" name="pseudo" class="account-input" required>
+
+            <button type="submit" class="cta-button3">Enregistrer</button>
+            </form>
+        </div>
+    </div>
+</section>
+<section class="table">
+  <div class="row table-header">
+    <div>Photo</div>
+    <div>Titre</div>
+    <div>Auteur</div>
+    <div>Description</div>
+    <div>Disponibilité</div>
+    <div>Action</div>
+  </div>
+
+  <div class="row">
+    <div class="photo">
+      <img src="/TomTroc/img/section_image.jpg" alt="Livre">
+    </div>
+    <div>The Kinfolk Table</div>
+    <div>Nathan Williams</div>
+    <div class="table-description">
+      J'ai récemment plongé dans les pages de "The Kinfolk Table" et j'ai été enchanté p...
+    </div>
+    <div>
+      <span class="badge available">disponible</span>
+    </div>
+    <div class="actions">
+      <a href="#">Éditer</a>
+      <a href="#" class="delete">Supprimer</a>
+    </div>
+  </div>
+
+  <div class="row alt">
+    <div class="photo">
+      <img src="/TomTroc/img/section_image.jpg" alt="Livre">
+    </div>
+    <div>The Kinfolk Table</div>
+    <div>Nathan Williams</div>
+    <div class="table-description">
+      J'ai récemment plongé dans les pages de "The Kinfolk Table" et j'ai été enchanté p...
+    </div>
+    <div>
+      <span class="badge unavailable">non dispo.</span>
+    </div>
+    <div class="actions">
+      <a href="#">Éditer</a>
+      <a href="#" class="delete">Supprimer</a>
+    </div>
+  </div>
+</section>
+
+<?php include __DIR__ . '/footer.php'; ?>
