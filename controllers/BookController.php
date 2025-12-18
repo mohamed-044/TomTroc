788efda0ab0af761a7909e5bfece86cd829a7c8f
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-12-18 20:50:06 +0100

    function to show home page

---
+++ controllers/BookController.php
@@ -0,0 +1,18 @@
+<?php 
+
+class BookController 
+{
+    /**
+     * Affiche la page d'accueil.
+     * @return void
+     */
+    public function showHome() : void
+    {
+        $bookManager = new BookManager();
+        $books = $bookManager->getAllBooks();
+
+        $view = new View("Accueil");
+        $view->render("main", ['books' => $books]);
+    }
+
+ }
