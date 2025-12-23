92b87148fedfba02aa95e6e98127e95efa4df6b1
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-12-23 16:23:17 +0100

    show home page and exchange books page depending on user action

--- index.php
+++ index.php
@@ -3,7 +3,33 @@ require_once 'config/config.php';
 require_once 'config/autoload.php';
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
-$bookController = new BookController();
+
+// On récupère l'action demandée par l'utilisateur.
+// Si aucune action n'est demandée, on affiche la page d'accueil.
+$action = Utils::request('action', 'home');
+
+// Try catch global pour gérer les erreurs
+try {
+    // Pour chaque action, on appelle le bon contrôleur et la bonne méthode.
+    switch ($action) {
+        // Pages accessibles à tous.
+        case 'home':
+            $bookController = new BookController();
             $bookController->showHome();
+            break;
+
+        case 'books':
+            $bookController = new BookController();
+            $bookController->showBooks();
+            break;
+
+        default:
+            throw new Exception("La page demandée n'existe pas.");
+    }
+} catch (Exception $e) {
+    // En cas d'erreur, on affiche la page d'erreur.
+    $errorView = new View('Erreur');
+    $errorView->render('errorPage', ['errorMessage' => $e->getMessage()]);
+}            
 ?>
 
