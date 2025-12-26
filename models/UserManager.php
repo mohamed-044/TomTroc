9863bd209ffb9e6d90aeb6cba3b757170d17619d
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-12-27 00:29:39 +0100

    add method to get user by id

--- models/UserManager.php
+++ models/UserManager.php
@@ -7,6 +7,22 @@
 class UserManager extends AbstractEntityManager 
 {
     /**
+     * Récupère un user par son id.
+     * @param int $id
+     * @return ?User
+     */
+    public function getUserById(int $id) : ?User 
+    {
+        $sql = "SELECT * FROM user WHERE id = :id";
+        $result = $this->db->query($sql, ['id' => $id]);
+        $user = $result->fetch();
+        if ($user) {
+            return new User($user);
+        }
+        return null;
+    }
+
+    /**
      * Récupère un user par son login.
      * @param string $login
      * @return ?User
@@ -20,5 +36,22 @@ class UserManager extends AbstractEntityManager
             return new User($user);
         }
         return null;
+    }
+
+    /**
+     * Met à jour un user.
+     * @param User $user
+     * @return void
+     */
+    public function updateUser(User $user) : void 
+    {
+        $sql = "UPDATE user SET login = :login, password = :password, name = :name, image = :image WHERE id = :id";
+        $this->db->query($sql, [
+            'login' => $user->getLogin(),
+            'password' => $user->getPassword(),
+            'name' => $user->getName(),
+            'image' => $user->getImage(),
+            'id' => $user->getId()
+        ]);
     }
 }
