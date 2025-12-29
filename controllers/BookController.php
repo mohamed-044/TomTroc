fbe41b7d4379c057cfc96db757091b4dae37b0bd
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-12-29 20:33:39 +0100

    add methods to show edit book form, delete book and update book

--- controllers/BookController.php
+++ controllers/BookController.php
@@ -30,5 +30,69 @@ class BookController
             exit;
         }
     }
-}
 
+    /**
+     * Affiche le formulaire d'édition d'un livre.
+     * @return void
+     */
+    public function showEditBookForm() : void
+    {
+        $userController = new UserController();
+        $userController->checkIfUserIsConnected();
+        $bookId = isset($_GET['id']) ? (int)$_GET['id'] : -1;
+        $bookManager = new BookManager();
+        $book = $bookManager->getBookById($bookId);
+        if (!$book) {
+            throw new Exception("Livre non trouvé.");
+        }
+        $view = new View("Édition du livre");
+        $view->render("editBookForm", ['book' => $book]);
+    }
+    /**
+     * Supprime un livre.
+     * @return void
+     */
+    public function deleteBook() : void
+    {
+        $userController = new UserController();
+        $userController->checkIfUserIsConnected();
+        $bookId = isset($_GET['id']) ? (int)$_GET['id'] : -1;
+        $bookManager = new BookManager();
+        $book = $bookManager->getBookById($bookId);
+        if (!$book) {
+            throw new Exception("Livre non trouvé.");
+        }
+        $bookManager->deleteBook($bookId);
+        header("Location: index.php?action=books");
+        exit;
+    }
+
+    /**
+     * Met à jour un livre.
+     * @return void
+     */
+    public function updateBook() : void
+    {
+        $userController = new UserController();
+        $userController->checkIfUserIsConnected();
+
+        $bookId = (int)$_POST['id'];
+
+        $bookManager = new BookManager();
+        $book = $bookManager->getBookById($bookId);
+
+        if (!$book) {
+            throw new Exception("Livre non trouvé.");
+        }
+
+        $book->setTitle($_POST['title']);
+        $book->setAuthor($_POST['author']);
+        $book->setDescription($_POST['description']);
+        $book->setStatus($_POST['status']);
+
+        $bookManager->updateBook($book);
+
+        header("Location: index.php?action=account");
+        exit;
+    }
+}
