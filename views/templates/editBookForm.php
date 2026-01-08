09130ab89fb5ef626fd71887a0e45efe1658392b
mohamed-044 <mohamed.boulatika04@gmail.com>
2026-01-08 18:48:11 +0100

    update edit book form

--- views/templates/editBookForm.php
+++ views/templates/editBookForm.php
@@ -8,35 +8,42 @@
 </head>
 <body>
 <?php include __DIR__ . '/header.php'; ?>
-
+<section class="edit-book-section">
+    <a class="back-link" href="index.php?action=account"><-retour</a>
+    <h1 class="title7">Modifier les informations</h1>    
 <div class="edit-book-container">
-    <h2>Modifier le livre</h2>
+    <div class="edit-book-image">
+        <p class="author">Photo</p>
+        <img id="edit_book_image" src="./img/<?php echo htmlspecialchars($book->getImage() ?? ''); ?>" alt="Edit Book Image">
+        <a href="index.php?action=editBookImage&id=<?= $book->getId() ?>" id="edit-book-link">Modifier la photo</a>
+    </div>
+
 
     <form class="edit-book-form" action="index.php?action=updateBook" method="post" enctype="multipart/form-data">
 
         <input type="hidden" name="id" value="<?= $book->getId() ?>">
 
-        <label>Titre</label>
-        <input type="text" name="title" value="<?= $book->getTitle() ?>">
+        <label class="author" >Titre</label>
+        <input type="text" name="title" id="edit-title" value="<?= $book->getTitle() ?>">
 
-        <label>Auteur</label>
-        <input type="text" name="author" value="<?= $book->getAuthor() ?>">
+        <label class="author" >Auteur</label>
+        <input type="text" name="author" id="edit-author" value="<?= $book->getAuthor() ?>">
 
-        <label>Description</label>
-        <textarea name="description"><?= $book->getDescription() ?></textarea>
+        <label class="author" >Commentaire</label>
+        <textarea name="description" id="edit-description"><?= $book->getDescription() ?></textarea>
 
-        <label>Disponibilité</label>
-        <select name="status">
+        <label class="author" >Disponibilité</label>
+        <select name="status" id="edit-disponibility">
             <option value="true" <?= $book->getStatus() === "true" ? "selected" : "" ?>>Disponible</option>
             <option value="false" <?= $book->getStatus() === "false" ? "selected" : "" ?>>Indisponible</option>
         </select>
 
-        <button type="submit">Enregistrer</button>
+        <button type="submit" class="cta-button8">Valider</button>
     </form>
 
-    <a class="back-link" href="index.php?action=account">Retour</a>
+    
 </div>
-
+</section>
 <?php include __DIR__ . '/footer.php'; ?>
 </body>
 </html>
